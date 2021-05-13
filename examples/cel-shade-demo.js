afa01e316ce16a2647647b7e211dff67b4b33029
OlayColay <colestrain2000@gmail.com>
2021-05-13 13:46:24 -0700

    Outlines! Instructions for use are in examples/cel-shade-demo

--- examples/cel-shade-demo.js
+++ examples/cel-shade-demo.js
@@ -28,6 +28,8 @@ export class Cel_Shade_Demo extends Scene {
                 {ambient: .6, diffusivity: .5, specularity: 0, low_threshold: -0.05, high_threshold: 0.05, color: hex_color("#ffffff")}),
             background: new Material(new defs.Phong_Shader(),
                 {ambient: 1, diffusivity: 0, specularity: 0, color: hex_color("#b0b0b0")}),
+            outline: new Material(new defs.Phong_Shader(),
+                {ambient: 0, diffusivity: 0, specularity: 0, color: hex_color("#000000")}),
         }
         // If N*L is under low_threshold, diffused light is 0.
         // If N*L is over high_threshold, diffused light is maxed.
@@ -47,6 +49,12 @@ export class Cel_Shade_Demo extends Scene {
             program_state.set_camera(this.initial_camera_location);
         }
 
+        // Set frontface culling for making outlines.
+        // Enable culling for next drawn objects with gl.enable(gl.CULL_FACE)
+        // Disable culling with gl.disable(gl.CULLFACE)
+        var gl = context.context;
+        gl.cullFace(gl.BACK);
+
         program_state.projection_transform = Mat4.perspective(
             Math.PI / 4, context.width / context.height, .1, 1000);
 
@@ -58,14 +66,32 @@ export class Cel_Shade_Demo extends Scene {
         // Cel Sphere
         let model_transform = Mat4.identity();
         this.shapes.sphere.draw(context, program_state, model_transform, this.materials.sphere.override({color: [1, 0, 0, 1]}));
+        // Outline for Cel Sphere
+        gl.enable(gl.CULL_FACE);
+        let outline_transform = model_transform.times(Mat4.scale(1.05, 1.05, 1.05));
+        this.shapes.sphere.draw(context, program_state, outline_transform, this.materials.outline);
+        gl.disable(gl.CULL_FACE);
 
         // Cel Axis
         model_transform = model_transform.times(Mat4.translation(2, -1, -1));
         this.shapes.axis.draw(context, program_state, model_transform, this.materials.axis.override({color: hex_color("#c7aa5b")}));
+        // Outlining composite shapes, like the axis, does not work because scaling doesn't work
+        gl.enable(gl.CULL_FACE);
+        outline_transform = model_transform.times(Mat4.scale(1.05, 1.05, 1.05));
+        this.shapes.axis.draw(context, program_state, outline_transform, this.materials.outline);
+        gl.disable(gl.CULL_FACE);
 
         // Cel Donut
         model_transform = model_transform.times(Mat4.translation(-4.5, 1, 1)).times(Mat4.rotation(t, 1, 2, 0));
         this.shapes.torus.draw(context, program_state, model_transform, this.materials.axis.override({color: hex_color("#ff52dc")}));
+        // Outline for Cel Donut
+        gl.enable(gl.CULL_FACE);
+        outline_transform = model_transform.times(Mat4.scale(1.05, 1.05, 1.05));
+        this.shapes.torus.draw(context, program_state, outline_transform, this.materials.outline);
+        // Interior lines for Cel Donut
+        outline_transform = model_transform.times(Mat4.scale(0.9, 0.9, 0.9));
+        this.shapes.torus.draw(context, program_state, outline_transform, this.materials.outline);
+        gl.disable(gl.CULL_FACE);
 
         // White background
         model_transform = Mat4.identity().times(Mat4.translation(0, 0, -5)).times(Mat4.scale(100, 100, 100));
