6bcd5f9aac024e81641cb802df42b920b59cfa2f
OlayColay <colestrain2000@gmail.com>
2021-05-14 15:58:04 -0700

    Textured cel shading

--- examples/cel-shade-demo.js
+++ examples/cel-shade-demo.js
@@ -1,7 +1,8 @@
 import {defs, tiny} from './common.js';
+import {Shape_From_File} from './obj-file-demo.js'
 
 const {
-    Vector, Vector3, vec, vec3, vec4, color, hex_color, Shader, Matrix, Mat4, Light, Shape, Material, Scene,
+    Vector, Vector3, vec, vec3, vec4, color, hex_color, Shader, Texture, Matrix, Mat4, Light, Shape, Material, Scene,
 } = tiny;
 
 class Suzanne extends Shape {
@@ -28,6 +29,7 @@ export class Cel_Shade_Demo extends Scene {
             sphere: new defs.Subdivision_Sphere(4),
             axis: new defs.Axis_Arrows(),
             suzanne: new Suzanne(),
+            yoyo: new Shape_From_File("assets/Yoyo/yoyo.obj"),
         };
 
         // *** Materials
@@ -45,6 +47,11 @@ export class Cel_Shade_Demo extends Scene {
                 {ambient: 1, diffusivity: 0, specularity: 0, color: hex_color("#b0b0b0")}),
             outline: new Material(new defs.Phong_Shader(),
                 {ambient: 0, diffusivity: 0, specularity: 0, color: hex_color("#000000")}),
+            yoyo: new Material(new defs.Textured_Cel(), {
+                color: color(.1,.1,.1, 1), ambient: 0.8, diffusivity: .4, specularity: 0,
+                low_threshold: 0.1, high_threshold: 0.11, 
+                texture: new Texture("assets/Yoyo/796630369.png")
+            }),
         }
         // If N*L is under low_threshold, diffused light is 0.
         // If N*L is over high_threshold, diffused light is maxed.
@@ -52,7 +59,14 @@ export class Cel_Shade_Demo extends Scene {
         // Same idea with specularity for N*H and low_specular & high_specular
         // For reference, N*L = 0 is when the light is perpendicular to the normal.
 
+        this.paused = false;
+        this.t = 0.0;
         this.initial_camera_location = Mat4.look_at(vec3(0, 0, 5), vec3(0, 0, 0), vec3(0, 1, 0));
+    }
+
+    make_control_panel() {
+        // Draw the scene's buttons, setup their actions and keyboard shortcuts, and monitor live measurements.
+        this.key_triggered_button("Pause light revolution", ["e"], () => this.paused = !this.paused);
     }
 
     display(context, program_state) {
@@ -74,10 +88,12 @@ export class Cel_Shade_Demo extends Scene {
         program_state.projection_transform = Mat4.perspective(
             Math.PI / 4, context.width / context.height, .1, 1000);
 
-        const t = program_state.animation_time / 1000, dt = program_state.animation_delta_time / 1000;
+        if (!this.paused) {
+            this.t = program_state.animation_time / 1000;
+        }
 
         // Directional light from right-top-front
-        program_state.lights = [new Light(vec4(1, 1, 1, 0), color(1, 1, 1, 1), 1000)];
+        program_state.lights = [new Light(vec4(Math.sin(this.t), 1, Math.cos(this.t), 0), color(1, 1, 1, 1), 100000)];
 
         // Cel Sphere
         let model_transform = Mat4.identity();
@@ -92,13 +108,13 @@ export class Cel_Shade_Demo extends Scene {
         model_transform = model_transform.times(Mat4.translation(2, -1, -1));
         this.shapes.axis.draw(context, program_state, model_transform, this.materials.axis.override({color: hex_color("#c7aa5b")}));
         // Outlining does not work because some faces are inside-out
-        gl.enable(gl.CULL_FACE);
-        outline_transform = model_transform.times(Mat4.scale(1.05, 1.05, 1.05));
-        this.shapes.axis.draw(context, program_state, outline_transform, this.materials.outline);
-        gl.disable(gl.CULL_FACE);
+        // gl.enable(gl.CULL_FACE);
+        // outline_transform = model_transform.times(Mat4.scale(1.05, 1.05, 1.05));
+        // this.shapes.axis.draw(context, program_state, outline_transform, this.materials.outline);
+        // gl.disable(gl.CULL_FACE);
 
         // Cel Donut
-        model_transform = model_transform.times(Mat4.translation(-4.5, 1, 1)).times(Mat4.rotation(t, 1, 2, 0));
+        model_transform = model_transform.times(Mat4.translation(-4.5, 1, 1)).times(Mat4.rotation(this.t, 1, 2, 0));
         this.shapes.torus.draw(context, program_state, model_transform, this.materials.axis.override({color: hex_color("#ff52dc")}));
         // Outline for Cel Donut
         gl.enable(gl.CULL_FACE);
@@ -120,6 +136,18 @@ export class Cel_Shade_Demo extends Scene {
         // Interior lines for Cel Suzanne
         outline_transform = model_transform.times(Mat4.scale(0.9, 0.9, 0.9));
         this.shapes.suzanne.draw(context, program_state, outline_transform, this.materials.outline);
+        gl.disable(gl.CULL_FACE);
+
+        // Textured human (Just ignore the head. I don't know how to use multiple textures)
+        model_transform = model_transform.times(Mat4.translation(10, 0.5, 0)).times(Mat4.scale(0.9, 0.9, 0.9));
+        this.shapes.yoyo.draw(context, program_state, model_transform, this.materials.yoyo);
+        // Outlines
+        gl.enable(gl.CULL_FACE);
+        outline_transform = model_transform.times(Mat4.scale(1.02, 1.02, 1.02));
+        this.shapes.yoyo.draw(context, program_state, outline_transform, this.materials.outline);
+        // Interior lines
+        outline_transform = model_transform.times(Mat4.scale(0.97, 0.97, 0.97));
+        this.shapes.yoyo.draw(context, program_state, outline_transform, this.materials.outline);
         gl.disable(gl.CULL_FACE);
 
         // White background
--- examples/common.js
+++ examples/common.js
@@ -962,6 +962,65 @@ const Textured_Phong = defs.Textured_Phong =
         }
     }
 
+const Textured_Cel = defs.Textured_Cel =
+    class Textured_Cel extends Cel_Shader {
+        // **Textured_Cel** is a Cel Shader extended to addditionally decal a
+        // texture image over the drawn shape, lined up according to the texture
+        // coordinates that are stored at each shape vertex.
+        vertex_glsl_code() {
+            // ********* VERTEX SHADER *********
+            return this.shared_glsl_code() + `
+                varying vec2 f_tex_coord;
+                attribute vec3 position, normal;                            
+                // Position is expressed in object coordinates.
+                attribute vec2 texture_coord;
+                
+                uniform mat4 model_transform;
+                uniform mat4 projection_camera_model_transform;
+        
+                void main(){                                                                   
+                    // The vertex's final resting place (in NDCS):
+                    gl_Position = projection_camera_model_transform * vec4( position, 1.0 );
+                    // The final normal vector in screen space.
+                    N = normalize( mat3( model_transform ) * normal / squared_scale);
+                    vertex_worldspace = ( model_transform * vec4( position, 1.0 ) ).xyz;
+                    // Turn the per-vertex texture coordinate into an interpolated variable.
+                    f_tex_coord = texture_coord;
+                  } `;
+        }
+
+        fragment_glsl_code() {
+            // ********* FRAGMENT SHADER *********
+            // A fragment is a pixel that's overlapped by the current triangle.
+            // Fragments affect the final image or get discarded due to depth.
+            return this.shared_glsl_code() + `
+                varying vec2 f_tex_coord;
+                uniform sampler2D texture;
+        
+                void main(){
+                    // Sample the texture image in the correct place:
+                    vec4 tex_color = texture2D( texture, f_tex_coord );
+                    if( tex_color.w < .01 ) discard;
+                                                                             // Compute an initial (ambient) color:
+                    gl_FragColor = vec4( ( tex_color.xyz + shape_color.xyz ) * ambient, shape_color.w * tex_color.w ); 
+                                                                             // Compute the final color with contributions from lights:
+                    gl_FragColor.xyz += phong_model_lights( normalize( N ), vertex_worldspace );
+                  } `;
+        }
+
+        update_GPU(context, gpu_addresses, gpu_state, model_transform, material) {
+            // update_GPU(): Add a little more to the base class's version of this method.
+            super.update_GPU(context, gpu_addresses, gpu_state, model_transform, material);
+
+            if (material.texture && material.texture.ready) {
+                // Select texture unit 0 for the fragment shader Sampler2D uniform called "texture":
+                context.uniform1i(gpu_addresses.texture, 0);
+                // For this draw, use the texture image from correct the GPU buffer:
+                material.texture.activate(context);
+            }
+        }
+    }
+
 
 const Fake_Bump_Map = defs.Fake_Bump_Map =
     class Fake_Bump_Map extends Textured_Phong {
