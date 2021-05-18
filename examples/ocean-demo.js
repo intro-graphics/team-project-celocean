5b054d3dfa75e49bb4d7e29c6e3d79361a418101
Vikram Peddinti <vikram.peddinti@gmail.com>
2021-05-18 15:03:18 -0700

    Wave Direction

--- examples/ocean-demo.js
+++ examples/ocean-demo.js
@@ -23,7 +23,7 @@ export class Ocean_Demo extends Scene {
             axis: new defs.Axis_Arrows(),
             square: new defs.Square(),
             cube: new defs.Cube(),
-            grid: new defs.Grid_Patch(15, 15, row_operation, column_operation),
+            grid: new defs.Grid_Patch(100, 100, row_operation, column_operation),
         };
 
         // *** Materials
@@ -55,7 +55,7 @@ export class Ocean_Demo extends Scene {
 
         this.paused = false;
         this.t = 0.0;
-        this.initial_camera_location = Mat4.look_at(vec3(0, 0, 61), vec3(0, 0, 0), vec3(0, 1, 0));
+        this.initial_camera_location = Mat4.look_at(vec3(4, 0, 48), vec3(0, 0, 0), vec3(0, 1, 0));
     }
 
     make_control_panel() {
@@ -89,12 +89,12 @@ export class Ocean_Demo extends Scene {
         // Directional light from right-top-front
         program_state.lights = [new Light(vec4(Math.sin(this.t), 1, Math.cos(this.t), 0), color(1, 1, 1, 1), 100000)];
 
-        let model_transform = Mat4.identity().times(Mat4.rotation(90, 1, 0, 0)).times(Mat4.scale(10,10,10));
+        let model_transform = Mat4.identity().times(Mat4.rotation(90,1,0,0)).times(Mat4.scale(2,2,2));
 
         //Interestingly, spheres are SMOOTHER
         //this.shapes.sphere.draw(context, program_state, model_transform, this.materials.ocean.override({color: [1, 0, 0, 1]}));
         //Drawing the wave grid!
-        this.shapes.grid.draw(context, program_state, model_transform, this.materials.ocean.override({color: [0.68, 0.85, 0.90, 1], amplitude: 1.0, wavelength: 4.0, time: this.t}));
+        this.shapes.grid.draw(context, program_state, model_transform, this.materials.ocean.override({color: [0.68, 0.85, 0.90, 1], amplitude: 0.2, wavelength: 5.0, time: this.t, speed: 3.0}));
 
         // White background
         model_transform = Mat4.identity().times(Mat4.translation(0, 0, -5)).times(Mat4.scale(100, 100, 100));
